09e4e0f3cd4cb38317f11bccd6c48158724a99ff
mattdibi <matthewdibi@gmail.com>
2017-02-24 12:16:07 +0100

    Added Pid on screen during tracking

--- main.cpp
+++ main.cpp
@@ -246,6 +246,7 @@ int main(int argc, char * argv[])
             if(passengers[i].getTracks().size() > 2)
             {
                 polylines(frame, passengers[i].getTracks(), false, passengers[i].getTrackColor(),3);
+                putText(frame, "Pid: " + to_string(passengers[i].getPid()), passengers[i].getCenter() , FONT_HERSHEY_SIMPLEX, 1,passengers[i].getTrackColor() , 2);
             }
 
             // --UPDATE PASSENGER STATS
